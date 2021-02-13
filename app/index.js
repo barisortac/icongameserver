8133a25bb1d6627c9cef6754b8896cb8fe0d287c
Barış Ortaç <baris.ortac@hamurlabs.com>
2021-02-13 19:57:54 +0300

    fix cors

--- app/index.js
+++ app/index.js
@@ -9,6 +9,7 @@ app.use(bodyParser());
 //   optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
 // }
 // app.use(cors(corsOptions))
+app.use(cors())
 
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
