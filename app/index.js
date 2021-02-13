000fb944acf240dad7017711f543c8b5005dabbd
Barış Ortaç <baris.ortac@hamurlabs.com>
2021-02-13 20:08:15 +0300

    try catch db conenction

--- app/index.js
+++ app/index.js
@@ -33,16 +33,21 @@ async function connectToDatabase(uri) {
     return cachedDb
   }
 
-  // If no connection is cached, create a new one
-  const client = await MongoClient.connect(uri, { useNewUrlParser: true })
+  try {
+    // If no connection is cached, create a new one
+    const client = await MongoClient.connect(uri, { useNewUrlParser: true })
 
-  // Select the database through the connection,
-  // using the database path of the connection string
-  const db = await client.db(url.parse(uri).pathname.substr(1))
+    // Select the database through the connection,
+    // using the database path of the connection string
+    const db = await client.db(url.parse(uri).pathname.substr(1))
 
-  // Cache the database connection and return the connection
-  cachedDb = db
-  return db
+    // Cache the database connection and return the connection
+    cachedDb = db
+    return db
+  } catch(err) {
+    console.log("Error on db connection")
+    console.log(err)
+  }
 }
 
 
